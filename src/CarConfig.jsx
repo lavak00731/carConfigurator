b87c243295043bf2c174eb9568f6109fde8329d1
Ezequiel <eolavaca@gmail.com>
2023-03-15 16:42:45 -0300

    First commit

---
+++ src/CarConfig.jsx
@@ -0,0 +1,11 @@
+export const CarConfig = () => {
+  return (
+    <div className="container">
+        <header>
+            <h1>Welcome to Car Configurator</h1>
+        </header>
+        
+
+    </div>
+  )
+}
