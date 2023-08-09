65992b9115dba587521c97165490f358eec22349
Ezequiel <eolavaca@gmail.com>
2023-08-09 09:24:00 -0300

    rendering basic personalization

--- src/pages/CarPersonalization.jsx
+++ src/pages/CarPersonalization.jsx
@@ -1,16 +1,19 @@
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Carousel from 'react-bootstrap/Carousel';
+import './../styles/personalization.css';
 
 export default function CarPersonalization() {
   const location = useLocation();
   const { cardata, features, pictures } = location.state;
-
+  const [key, setKey] = useState('front');
   const loopColor = (feature) => {
-    let colors = [];
-    feature.map( ( color ) => {
-      const btn = <button key={Date.now()} type="button" style={{backgroundColor: `${color}`}}></button>
+    let colors = [];        
+    feature.map( ( {colorId, colorData, colorName } = color  ) => {
+      console.log(colorId)
+      const btn = <button className='chooseStyle' key={colorId} type="button" style={{backgroundColor: `${colorData}`}}><span>{colorName}</span></button>
       colors.push(btn);
     });
 
@@ -20,7 +23,7 @@ export default function CarPersonalization() {
     let content = [];
     for (let item in features) {      
       const feature = features[item];
-      content.push(<Tab eventKey={ item } title={ item }>{loopColor(feature)}</Tab>);
+      content.push(<Tab eventKey={ item } key={item} title={ item }>{loopColor(feature)}</Tab>);
     }
     return content
   }
@@ -33,8 +36,9 @@ export default function CarPersonalization() {
         <div className="col-sm-12 col-lg-8 order-lg-2">cArousel</div>
         <div className="col-sm-12 col-lg-4 order-lg-1">
           <Tabs
-            defaultActiveKey={features['front']}
-            id="tabComp"
+            did="controlled-tab-example"
+            activeKey={key}
+            onSelect={(k) => setKey(k)}
             className="mb-3"
           >
             {
